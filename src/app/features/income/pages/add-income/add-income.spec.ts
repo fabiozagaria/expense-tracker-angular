77868ff96a1d40f83938fac937320e1baad72ac7
Fabio <fabiozagaria@proton.me>
2026-09-21 13:47:35 +0200

    test: adapt income and dashboard specs

--- src/app/features/income/pages/add-income/add-income.spec.ts
+++ src/app/features/income/pages/add-income/add-income.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
 
 import { AddIncome } from './add-income';
+import { IncomeService } from '../../services/income.service';
 
 describe('AddIncome', () => {
   let component: AddIncome;
@@ -10,7 +12,10 @@ describe('AddIncome', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddIncome],
-      providers: [provideRouter([])],
+      providers: [
+        provideRouter([]),
+        { provide: IncomeService, useValue: { addIncome: () => of({}) } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddIncome);

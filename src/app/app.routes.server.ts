369492f238762e2443049d996ce38636bd871763
Fabio <fabiozagaria@proton.me>
2026-09-21 13:45:39 +0200

    feat: connect dashboard to income and expense totals

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'dashboard',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'add-transaction',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'add-income',
+    renderMode: RenderMode.Client
+  },
+  {
     path: 'add-expense',
     renderMode: RenderMode.Client
   },
